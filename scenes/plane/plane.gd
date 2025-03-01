855cdbf4493e6eb588f0cd4fc947c4f0351f80ec
greg-swinbanks <greg.swinbanks@clad.nz>
2025-02-28 22:38:39 -0500

    Make the plane fly

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const GRAVITY: float = 600.0
+const POWER: float = -200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +12,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
+	fly()
+	
 	move_and_slide()
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
