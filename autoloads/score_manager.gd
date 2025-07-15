3a1fa204aaff2e672271522ee060eee4cc60c03a
greg-swinbanks <greg.swinbanks@clad.nz>
2025-07-14 20:55:44 -0400

    Saving highscores

--- autoloads/score_manager.gd
+++ autoloads/score_manager.gd
@@ -1,12 +1,14 @@
 extends Node
 
 
+const SCORES_PATH = "user://tappy.dat"
+
 var _score: int = 0
 var _high_score: int = 0
 
 
 func _ready() -> void:
-	pass 
+	load_high_score()
 
 func get_score() -> int:
 	return _score
@@ -22,3 +24,21 @@ func set_score(v: int) -> void:
 
 func increment_score() -> void:
 	set_score(_score + 1)
+
+func load_high_score() -> void:
+	var file: FileAccess = FileAccess.open(SCORES_PATH, FileAccess.READ)
+	if file:
+		if file.get_length() > 0:
+			_high_score = file.get_32()
+			print("Loaded high score")
+		else:
+			print("Nothing in scores file...")
+		file.close()
+	else:
+		print("Failed to load file...")
+
+func save_high_score_to_file() -> void:
+	var file: FileAccess = FileAccess.open(SCORES_PATH, FileAccess.WRITE)
+	if file:
+		file.store_32(_high_score)
+		file.close()
